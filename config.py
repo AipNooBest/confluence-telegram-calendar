407222724972aafaae18639094612a17a5b2cd27
AipNooBest <47401054+aipnoobest@users.noreply.github.com>
2023-03-03 15:14:15 +0300

    Перенос конфигурации из .env в config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-from os import environ
-from dotenv import load_dotenv
-load_dotenv(".env")
-
-CONF_ADDRESS = environ.get("CONF_ADDRESS")
+# Адрес самого Confluence, например http://192.168.1.2:8080
+CONF_ADDRESS = ""
 CONF_API = CONF_ADDRESS + '/rest/api'
-TELEGRAM_KEY = environ.get("TELEGRAM_KEY")
-CALENDAR_PAGE = environ.get("CALENDAR_PAGE_ID")
+TELEGRAM_KEY = ""
+# Номер страницы, где находится календарь (например, 12345678)
+CALENDAR_PAGE = ""
 
-CONF_LOGIN = environ.get("CONF_LOGIN")
-CONF_PASSWORD = environ.get("CONF_PASSWORD")
+# Сервисный аккаунт для редактирования календаря
+CONF_LOGIN = ""
+CONF_PASSWORD = ""
 
-DB_HOST = environ.get("DB_HOST")
-DB_PORT = environ.get("DB_PORT")
+# Где развёрнута MongoDB
+DB_HOST = "localhost"
+DB_PORT = "27017"
